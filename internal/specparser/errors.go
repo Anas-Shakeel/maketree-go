56eac1bcee68518fdbe7499461c90700fc35c8a0
Anas Shakeel <srdranas456@gmail.com>
2025-12-16 06:40:20 +0500

    Add ErrInvalidIndent error

--- internal/specparser/errors.go
+++ internal/specparser/errors.go
@@ -1,1 +1,17 @@
-package parser
+package specparser
+
+import "fmt"
+
+// ErrInvalidIndent occurs when indentation is inconsistent/invalid
+type ErrInvalidIndent struct {
+	Line   int
+	Found  int // Number of indent spaces found
+	Reason string
+}
+
+func (e *ErrInvalidIndent) Error() string {
+	return fmt.Sprintf(
+		"invalid indentation at line %d: %s (found %d spaces)",
+		e.Line, e.Reason, e.Found,
+	)
+}
