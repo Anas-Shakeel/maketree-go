29a1397ebe4103b85c028f59ce1327f08501f9db
Anas Shakeel <srdranas456@gmail.com>
2025-12-16 06:43:54 +0500

    Setup basic structure for parsing and returning parsed tree or errors

--- internal/specparser/parser.go
+++ internal/specparser/parser.go
@@ -1,4 +1,57 @@
 // parser implements the parser that parses source spec file
-package parser
+package specparser
 
-// TODO: Implement the parser
+import (
+	"fmt"
+	"os"
+
+	"github.com/anas-shakeel/maketree-go/internal/models"
+)
+
+// Parse parses the file and returns a *Node tree or an error.
+//
+// filename: name of the file that defines a valid file structure
+func Parse(filename string) (*models.Node, error) {
+	// Read the file
+	contents, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	// Parse the file
+	nodetree, err := ParseString(string(contents))
+	if err != nil {
+		return nil, err
+	}
+
+	return nodetree, nil
+
+}
+
+// Parse parses the input string (spec tree) and returns a *Node tree or an error
+//
+// input: the input string that defines a valid file structure
+func ParseString(input string) (*models.Node, error) {
+	// Tokenize the input
+	tokens, err := tokenize(input)
+	if err != nil {
+		return nil, err
+	}
+
+	// Parse the tokens
+	nodetree, err := parseTokens(tokens)
+	if err != nil {
+		return nil, err
+	}
+
+	return nodetree, nil
+
+}
+
+// parseTokens parses the tokens and returns a pointer to Node tree
+func parseTokens(tokens *[]Token) (*models.Node, error) {
+	// TODO: Implement the parsing logic
+	fmt.Printf("%#v\n", tokens)
+
+	return &models.Node{}, nil
+}
